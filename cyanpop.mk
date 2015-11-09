e0d764d10f292b679ea0363f9099e617a94c6f32
RolanDroid <rolandosimoncelli8@gmail.com>
2015-11-09 22:15:44 +0100

    CyanPop 5.1.1...
    Signed-off-by: RolanDroid <rolandosimoncelli8@gmail.com>

--- cyanpop.mk
+++ cyanpop.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/motorola/thea/full_thea.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common CYANPOP stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := thea
-PRODUCT_NAME := cm_thea
+PRODUCT_NAME := cyanpop_thea
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
